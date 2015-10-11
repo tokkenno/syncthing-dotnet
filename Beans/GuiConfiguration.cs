162aeb6cc4c7861f319cb18d9d9e3360920fd5ae
Aitor González Fernández <reimashi@gmail.com>
2015-10-11 19:11:19 +0200

    Added options/gui fields

--- Beans/GuiConfiguration.cs
+++ Beans/GuiConfiguration.cs
@@ -9,5 +9,52 @@ namespace SyncthingApi.Beans
     [DataContract]
     public class GuiConfiguration
     {
+        [DataMember(Name = "enabled", IsRequired = true, EmitDefaultValue = true)]
+        private Boolean enabled;
+
+        public Boolean Enabled
+        {
+            get { return this.enabled; }
+        }
+
+        [DataMember(Name = "address", IsRequired = true, EmitDefaultValue = false)]
+        private string address;
+
+        public string Address
+        {
+            get { return this.address; }
+        }
+
+        [DataMember(Name = "user", IsRequired = false, EmitDefaultValue = false)]
+        private string user;
+
+        public string User
+        {
+            get { return this.user; }
+        }
+
+        [DataMember(Name = "password", IsRequired = false, EmitDefaultValue = false)]
+        private string password;
+
+        public string Password
+        {
+            get { return this.password; }
+        }
+
+        [DataMember(Name = "useTLS", IsRequired = false)]
+        private Boolean useTLS;
+
+        public Boolean UseTLS
+        {
+            get { return this.useTLS; }
+        }
+
+        [DataMember(Name = "apiKey", IsRequired = false, EmitDefaultValue = false)]
+        private String apiKey;
+
+        public String ApiKey
+        {
+            get { return this.apiKey; }
+        }
     }
 }
