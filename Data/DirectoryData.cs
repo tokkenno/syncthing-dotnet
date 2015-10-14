a2d412e04d17a1277b0abad48a7eeb4bbef29d2f
Aitor González Fernández <reimashi@gmail.com>
2015-10-15 01:21:51 +0200

    Method GET /system/db/browse implemented

---
+++ Data/DirectoryData.cs
@@ -0,0 +1,78 @@
+﻿using Syncthing.API.Utils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Syncthing.API.Data
+{
+    public class DirectoryData
+    {
+        private DirectoryData(String name) { this.name = name; }
+
+        private string name;
+        public String Name {  get { return this.name; } }
+
+        private List<DirectoryData> directories = new List<DirectoryData>();
+        public List<DirectoryData> Directories { get { return this.directories; } }
+
+        private List<FileData> files = new List<FileData>();
+        public List<FileData> Files { get { return this.files; } }
+
+        public static DirectoryData DeserializeJson(string json)
+        {
+            using (MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(json)))
+            {
+                XmlReader reader = JsonReaderWriterFactory.CreateJsonReader(ms, XmlDictionaryReaderQuotas.Max);
+                XElement root = XElement.Load(reader);
+
+                DirectoryData tret = DeserializeDirectoryXml("/", root);
+                return tret;
+            }
+        }
+
+        protected static DirectoryData DeserializeDirectoryXml(String dir, XElement root)
+        {
+            DirectoryData dd = new DirectoryData(dir);
+
+            foreach (XElement elem in root.Elements())
+            {
+                XAttribute iname = elem.Attribute("item");
+                string name = iname == null ? elem.Name.LocalName : iname.Value;
+
+                if (elem.Attribute("type").Value == "object")
+                {
+                    dd.directories.Add(DeserializeDirectoryXml(name, elem));
+                }
+                else if (elem.Attribute("type").Value == "array")
+                {
+                    dd.files.Add(DeserializeFileXml(name, elem));
+                }
+                else
+                {
+                    throw new ApiException("Parser error. API respond is malformed.");
+                }
+            }
+
+            return dd;
+        }
+
+        protected static FileData DeserializeFileXml(String filename, XElement root)
+        {
+            XElement timenode = root.Elements().First<XElement>();
+            XElement sizenode = root.Elements().Skip<XElement>(1).First<XElement>();
+
+            if (timenode == null || sizenode == null) throw new ApiException("Parser error. API respond is malformed. File format error.");
+            else
+            {
+                return new FileData(filename, System.Convert.ToInt64(sizenode.Value), JavascriptFormatHelper.ConvertTime(timenode.Value));
+            }
+        }
+    }
+}
---
+++ Data/FileData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Syncthing.API.Data
+{
+    public class FileData
+    {
+        private string name;
+        private long size;
+        private DateTime modified;
+
+        public FileData(String name) : this(name, 0) { }
+
+        public FileData(String name, long size) : this(name, 0, DateTime.MinValue) { }
+
+        public FileData(String name, long size, DateTime modified)
+        {
+            this.name = name;
+            this.size = size;
+            this.modified = modified;
+        }
+
+        public String Name { get { return this.name; } }
+
+        public long Size { get { return this.size; } }
+
+        public DateTime Modified { get { return this.modified; } }
+    }
+}
