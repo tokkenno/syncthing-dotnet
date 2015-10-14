7894f2592eb2ea2f4cf74da0bf8cb0a4d95d96dc
Aitor González Fernández <reimashi@gmail.com>
2015-10-15 01:21:12 +0200

    Added javascript time formater

---
+++ Utils/JavascriptFormatHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Syncthing.API.Utils
+{
+    class JavascriptFormatHelper
+    {
+        public static DateTime ConvertTime(String jstime)
+        {
+            jstime = jstime.Remove(jstime.LastIndexOf(":"), 1);
+            return DateTime.ParseExact(jstime, "yyyy-MM-dd'T'HH:mm:ssK", System.Globalization.CultureInfo.InvariantCulture);
+        }
+    }
+}
